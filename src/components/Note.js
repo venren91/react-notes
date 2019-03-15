91d7efd2fbd4f54b061e0136530c3b589dc5c411
venren <47970317+venren91@users.noreply.github.com>
2019-03-15 17:25:15 +0100

    add style for notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Delete } from '@material-ui/icons';
 
 class Note extends Component{
 
     notes = []
-    notesTitleStyle = { fontSize: '20px' }
-    notesTextStyle = { fontSize: '15px' }
+    notesTitleStyle = { fontSize: '15px', border: 'none' }
+    notesTitleTextStyle = { paddingLeft: '5px', paddingRight: '5px', maxWidth: '85%'}
+    textAreaStyle = { overflow: 'hidden', resize: 'none', height:'5rem', border: 'none', outline: 'none', fontSize: '14px' }
+    cardContentCustom = { paddingLeft: '15px', paddingRight: '15px' }
+    cardActionCustom = { padding: '0px' }
+    materialIconColor = { color: 'black' }
 
     addNote = (e) => {
         this.notes.push({title:"Enter a title", text: "Enter the content here"})   
@@ -12,20 +17,22 @@ class Note extends Component{
 
     render(){
         return(
-            <div className="row">
-                <div className="col s4 m4">
-                <div className="card blue-grey darken-1">
-                    <div className="card-content white-text">
-                    <span className="card-title" style={this.notesTitleStyle}><input type="text"></input></span>
-                    <p style={this.notesTextStyle}>I am a very simple card. I am good at containing small bits of information.
-                    I am convenient because I require little markup to use effectively.</p>
-                    <div className="card-action">
-                    
+            <div className="container">
+                <div className="row">
+                    <div className="col s4 m4">
+                    <div className="card white-white darken-1">
+                        <span className="card-title" style={this.notesTitleStyle}><input type="text" style={this.notesTitleTextStyle}></input>
+                        </span>
+                        <div style={this.cardContentCustom}>
+                        <textarea rows="1" style={this.textAreaStyle}></textarea>
+                        </div>
+                        <div style={this.cardActionCustom}>
+                            <Delete style={this.materialIconColor}/>
+                        </div>
                     </div>
                     </div>
-                </div>
-                </div>
-            </div>    
+                </div>    
+            </div>
             
         );
     }
